8de5729c2f89c1ba612962538792e5870c3ee635
Your Name <you@example.com>
2017-05-10 14:29:20 -0500

    may 2017

--- UniformGenerator.hpp
+++ UniformGenerator.hpp
@@ -13,6 +13,7 @@ class UniformGenerator
       
    private:
       vector <int> generateFirstPrimes(int k);
+      vector<vector<double >> Transformation(vector< vector<double > > & designs, int dim);
 };
 #endif
 
@@ -49,42 +50,62 @@ vector <int> UniformGenerator::generateFirstPrimes(int k)
 vector<vector<double> > UniformGenerator::GenerateWeightVectors(int Objectives, int N)
 {
    vector<vector<double > > designs;
-   vector<int> primes = generateFirstPrimes(Objectives-2);
+   vector<int> primes = generateFirstPrimes(Objectives-1);
+
+
+ //     vector<double> design(Objectives, 0.0);
+ //     designs.push_back(design);
    for(int i = 0 ; i < N; i++)
    {
       vector<double> design(Objectives, 0.0);
-      design[0] = (2.0*(i+1.0) - 1.0) / (2.0*N);
-      for(int j = 1; j < Objectives-1; j++)
+      //design[0] = (2.0*(i+1.0) - 1.0) / (2.0*N);
+      design[0] = (double)( (i) % (N+1)) / (double) N;
+      for(int j = 1; j < Objectives; j++)
       {
 	   double f = 1.0/primes[j-1];
-	   int d = i+1;
+	   int d = i;
 	   design[j] = 0.0;
 	   while(d > 0)
 	   {
 		design[j] += f*(d % primes[j-1]);
-		d = d / primes[j-1]; // Here the floor is considered as part of the C++ language.....
+		d =   floor((double)d / primes[j-1]); // Here the floor is considered as part of the C++ language.....
 		f = f / primes[j-1];
 	   }
       }
 	designs.push_back(design);
    }
- // The designs are transformed to weight vectors....
-   vector<vector< double > > Weights;
-   for(int i = 0; i < designs.size(); i++)
-   {
-  	vector<double> Weight(Objectives); 
-	for(int j = 1; j <= Objectives; j++)
-	{
-	   if(j == Objectives)
-	 	Weight[j-1] = 1.0;
-	   else
-		Weight[j-1] = 1.0 - pow(designs[i][j-1], 1.0/(Objectives-j));
-	for(int k = 1; k <= j-1; k++)
-	       Weight[j-1] *=  pow( designs[i][k-1], 1.0/(Objectives-k) );
-	}
-	Weights.push_back(Weight);
-   } 
-   return Weights;
-}
 
+//
+//   for(int i = 0; i < N; i++)
+//	{
+//	for(int j = 0; j < Objectives; j++)
+//	cout << designs[i][j] << " ";
+//	cout << endl;
+//	}
+//cout << endl;
+ // The designs are transformed to weight vectors....
+   return Transformation(designs, Objectives); 
+}
+/**
+   Transformation Fang and Wang 1994
+**/
+ vector<vector<double> > UniformGenerator::Transformation(vector< vector<double > > & designs, int dim)
+{
+   vector<vector< double > > U;
+  for(int d = 0; d < designs.size(); d++)
+   {
+  	vector<double> Row(dim); 
+	for(int i = 1; i <= dim; i++)
+	{
+	   if(i == dim)
+	 	Row[i-1] = 1.0;
+	   else
+		Row[i-1] = 1.0 - pow(designs[d][i-1], 1.0/(dim-i));
+	for(int j = 1; j <= i-1; j++)
+	        Row[i-1] *=  pow( designs[d][j-1], 1.0/(dim-j) );
+	}
+	U.push_back(Row);
+   }
+	return U;
+}
 
